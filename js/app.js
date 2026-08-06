3c08a809faf356671825cb57a175c5971453a861
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-06 19:03:05 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+import {
+    db,
+    collection,
+    getDocs
+} from "./firebase.js";
 
+const tombol = document.getElementById("test");
+const hasil = document.getElementById("hasil");
+
+tombol.onclick = async () => {
+
+    try {
+
+        const snapshot = await getDocs(collection(db, "dokumen"));
+
+        hasil.innerHTML =
+            "Firebase terhubung. Jumlah dokumen: " +
+            snapshot.size;
+
+    } catch (err) {
+
+        console.error(err);
+
+        hasil.innerHTML =
+            "Gagal terhubung ke Firebase.";
+
+    }
+
+};
