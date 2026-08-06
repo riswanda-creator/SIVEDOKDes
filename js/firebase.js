bbd9fc7a60a2bd5352cf2eda00426b7c22832e13
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-06 19:00:23 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,39 +1,39 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+
 import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc,
-  collection,
-  getDocs,
-  query,
-  orderBy,
-  limit,
-  serverTimestamp
+    getFirestore,
+    doc,
+    setDoc,
+    getDoc,
+    getDocs,
+    collection,
+    query,
+    orderBy,
+    limit,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "ISI_DARI_FIREBASE",
-  authDomain: "ISI_DARI_FIREBASE",
-  projectId: "ISI_DARI_FIREBASE",
-  storageBucket: "ISI_DARI_FIREBASE",
-  messagingSenderId: "ISI_DARI_FIREBASE",
-  appId: "ISI_DARI_FIREBASE"
+    apiKey: "ISI_DARI_FIREBASE",
+    authDomain: "ISI_DARI_FIREBASE",
+    projectId: "ISI_DARI_FIREBASE",
+    storageBucket: "ISI_DARI_FIREBASE",
+    messagingSenderId: "ISI_DARI_FIREBASE",
+    appId: "ISI_DARI_FIREBASE"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 export {
-  db,
-  doc,
-  setDoc,
-  getDoc,
-  collection,
-  getDocs,
-  query,
-  orderBy,
-  limit,
-  serverTimestamp
+    db,
+    doc,
+    setDoc,
+    getDoc,
+    getDocs,
+    collection,
+    query,
+    orderBy,
+    limit,
+    serverTimestamp
 };
