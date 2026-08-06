5503af072c9309d05228b4b4f09d826baaab3afe
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-06 22:00:36 +0700

    Update app.js

--- admin/app.js
+++ admin/app.js
@@ -5,8 +5,7 @@ let nomor = 1;
 
 tombol.addEventListener("click", () => {
 
-    const tahun = new Date().getFullYear();
-
+    const tahun = new Date().getFullYear().toString();
     const documentId =
         `1219062002-${tahun}-${String(nomor).padStart(6,"0")}`;
 
