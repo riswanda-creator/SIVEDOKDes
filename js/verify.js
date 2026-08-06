470e6ff852bf621c907eed1b09db8dac19cadd35
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-06 21:34:52 +0700

    Create verify.js

---
+++ js/verify.js
@@ -0,0 +1,12 @@
+const status = document.getElementById("status");
+const detail = document.getElementById("detail");
+
+status.innerHTML = "🟢 HALAMAN VERIFIKASI SIAP";
+
+detail.innerHTML = `
+<p>
+Halaman ini akan menampilkan
+informasi dokumen setelah
+QR Code dipindai.
+</p>
+`;
