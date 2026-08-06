f02b7a294b4a275e504e0ea8da49c5f062963720
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-06 21:53:15 +0700

    Update app.js

--- admin/app.js
+++ admin/app.js
@@ -1,1 +1,17 @@
+const tombol = document.getElementById("registrasi");
+const hasil = document.getElementById("hasil");
 
+let nomor = 1;
+
+tombol.addEventListener("click", () => {
+
+    const tahun = new Date().getFullYear();
+
+    const documentId =
+        `1219062002-${tahun}-${String(nomor).padStart(6,"0")}`;
+
+    hasil.textContent = documentId;
+
+    nomor++;
+
+});
