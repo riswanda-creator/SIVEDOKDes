f06b64bc142080d64c4d7a4d67c147a6ab0bc061
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-06 19:18:09 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,46 @@
+import {
+    db,
+    collection,
+    getDocs,
+    query,
+    orderBy,
+    limit
+} from "./firebase.js";
+
 const hasil = document.getElementById("hasil");
 
-document.getElementById("generate").onclick = function () {
+async function getNomorTerakhir() {
+
+    const q = query(
+        collection(db, "dokumen"),
+        orderBy("nomorUrut", "desc"),
+        limit(1)
+    );
+
+    const snapshot = await getDocs(q);
+
+    if (snapshot.empty) {
+        return 1;
+    }
+
+    return snapshot.docs[0].data().nomorUrut + 1;
+}
+
+document.getElementById("generate").onclick = async function () {
 
     const jenis = document.getElementById("jenis").value;
 
     const tahun = document.getElementById("tahun").value;
 
-    const nomor = "000001";
+    const nomor = await getNomorTerakhir();
 
-    const id = `GT-${tahun}-${jenis}-${nomor}`;
+    const id =
+        "GT-" +
+        tahun +
+        "-" +
+        jenis +
+        "-" +
+        String(nomor).padStart(6, "0");
 
     hasil.innerHTML = id;
 
