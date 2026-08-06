bc51cb34244c5eb7d527194bb89bcd5835594c42
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-06 18:43:56 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "ISI_DARI_FIREBASE",
+  authDomain: "ISI_DARI_FIREBASE",
+  projectId: "ISI_DARI_FIREBASE",
+  storageBucket: "ISI_DARI_FIREBASE",
+  messagingSenderId: "ISI_DARI_FIREBASE",
+  appId: "ISI_DARI_FIREBASE"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {
+  db,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  serverTimestamp
+};
