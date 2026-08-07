5f426744e160510c850f8ebf7e18f5a61b257471
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-07 12:31:25 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,27 @@
+// =====================================================
+// SIVEDOKDes
+// KONFIGURASI IDENTITAS DESA
+// =====================================================
+
+export const DESA = {
+
+    nama: "Desa Guntung",
+
+    kode: "GT",
+
+    kecamatan: "Tanjung Tiram",
+
+    kabupaten: "Batu Bara",
+
+    provinsi: "Sumatera Utara",
+
+    kodeWilayah: "1219062002",
+
+    tahunAplikasi: 2026,
+
+    namaSistem: "SIVEDOKDes",
+
+    kepanjanganSistem:
+        "Sistem Verifikasi Dokumen Elektronik Desa"
+
+};
