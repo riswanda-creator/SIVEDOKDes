511626694f354764cff5c2a84385f6115494f97b
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-08 15:03:55 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -13,70 +13,91 @@ import {
     orderBy,
     limit,
     serverTimestamp,
-    runTransaction,
-    updateDoc
+    runTransaction
 } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
 import {
-    getStorage
+    getStorage,
+    ref,
+    uploadBytes,
+    getDownloadURL
 } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-storage.js";
 
 
+// =====================================================
+// FIREBASE CONFIG
+// =====================================================
+
 const firebaseConfig = {
 
-    apiKey: "AIzaSyDQUvjOMujFz6YbwaIC048D1wtq4anHkV0",
+    apiKey:
+        "AIzaSyDQUvjOMujFz6YbwaIC048D1wtq4anHkV0",
 
-    authDomain: "sivedokdes.firebaseapp.com",
+    authDomain:
+        "sivedokdes.firebaseapp.com",
 
-    projectId: "sivedokdes",
+    projectId:
+        "sivedokdes",
 
-    storageBucket: "sivedokdes.firebasestorage.app",
+    storageBucket:
+        "sivedokdes.firebasestorage.app",
 
-    messagingSenderId: "505099689462",
+    messagingSenderId:
+        "505099689462",
 
-    appId: "1:505099689462:web:086a511ee30169a3da7bd7"
+    appId:
+        "1:505099689462:web:086a511ee30169a3da7bd7"
 
 };
 
+
+// =====================================================
+// INITIALIZE FIREBASE
+// =====================================================
 
 const app =
     initializeApp(firebaseConfig);
 
 
+// =====================================================
+// FIRESTORE
+// =====================================================
+
 const db =
     getFirestore(app);
 
+
+// =====================================================
+// STORAGE
+// =====================================================
 
 const storage =
     getStorage(app);
 
 
+// =====================================================
+// EXPORT
+// =====================================================
+
 export {
 
+    // Firestore
     db,
-
-    storage,
-
     doc,
-
     setDoc,
-
-    updateDoc,
-
     getDoc,
-
     getDocs,
-
     collection,
-
     query,
-
     orderBy,
-
     limit,
-
     serverTimestamp,
+    runTransaction,
 
-    runTransaction
+    // Storage
+    storage,
+    ref,
+    uploadBytes,
+    getDownloadURL
 
 };
