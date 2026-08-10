5c60150af4f75f09ae1cf0a84d76e1dc7949c155
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-10 12:31:50 +0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,36 @@
+// =====================================================
+// SUPABASE CLIENT
+// SIVEDOKDes
+// =====================================================
+
+import {
+    createClient
+} from "https://esm.sh/@supabase/supabase-js@2";
+
+// =====================================================
+// SUPABASE CONFIG
+// =====================================================
+
+const SUPABASE_URL =
+    "https://ittfhjzkejhsbowwqlrq.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_javi5F477-dw8o3LD4YjHg_QejbW_cz";
+
+// =====================================================
+// INITIALIZE SUPABASE
+// =====================================================
+
+const supabase =
+    createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
+
+// =====================================================
+// EXPORT
+// =====================================================
+
+export {
+    supabase
+};
