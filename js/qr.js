d7c28f2018d3b3f8418084e37f18d90803e67530
riswanda-creator <muhammadarifinriswanda6123@gmail.com>
2026-08-10 14:27:33 +0700

    Update qr.js

--- js/qr.js
+++ js/qr.js
@@ -9,40 +9,62 @@ import QRCode from "https://esm.sh/qrcode@1.5.4";
 // BUAT QR CODE
 // =====================================================
 
-export async function buatQRCode(documentId, verifyURL) {
+export async function buatQRCode(
+    documentId,
+    verifyURL
+) {
 
     if (!documentId) {
+
         throw new Error(
             "Document ID tidak tersedia."
         );
+
     }
 
     if (!verifyURL) {
+
         throw new Error(
             "URL verifikasi tidak tersedia."
         );
+
     }
 
     const qrDataURL =
         await QRCode.toDataURL(
             verifyURL,
             {
-                errorCorrectionLevel: "H",
 
-                margin: 1,
+                errorCorrectionLevel:
+                    "H",
 
-                width: 500,
+                margin:
+                    1,
+
+                width:
+                    500,
 
                 color: {
-                    dark: "#000000",
-                    light: "#FFFFFF"
+
+                    dark:
+                        "#000000",
+
+                    light:
+                        "#FFFFFF"
+
                 }
+
             }
         );
 
     return {
+
         documentId,
+
         verifyURL,
+
         qrDataURL
+
     };
+
 }
